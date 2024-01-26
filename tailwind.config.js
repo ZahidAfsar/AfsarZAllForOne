512f68bb2c6d1fbbf5524bb6d8ea1c58557ff840
Zahid Afsar <zahidtempz@icloud.com>
2024-01-26 09:51:59 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html,js}",
+    "./pages/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Modak: ["Modak"],
+        Unlock: ["Unlock"]
+      },
+      colors: {
+        'custom-yellow':'#FAC05E',
+        'custom-red':'#EE6352',
+        'custom-blue':'#3FA7D6',
+        'custom-orange':'#F79D84',
+        'custom-green':'#59CD90',
+        'custom-purple': '#C59FC9',
+    },
+  },
+  plugins: [],
+}
+}
