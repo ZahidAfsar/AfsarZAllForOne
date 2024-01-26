b659ce910f27672550cb6139e04683e6ad03aa83
Zahid Afsar <zahidtempz@icloud.com>
2024-01-26 10:27:22 -0800

    pages

---
+++ Controllers/SayHelloController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AfsarZAllForOne.Controllers
+{
+    [Route("[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+       
+    }
+}
